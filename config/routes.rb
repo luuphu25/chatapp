4bcedf52c4c3c3f4e7142ec0df0dd8a4c30d3297
Ubuntu <ubuntu@rails-dev-box>
2017-02-23 15:04:42 +0000

    add session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/create'
-
+  resources :sessions, only: [:new, :create]
   resources :users
+
+  get 'sign_in' => 'sessions#new'
+  delete 'log_out' => 'sessions#destroy'  
   root 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
