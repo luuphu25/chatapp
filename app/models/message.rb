fb5ff16341cfcc26a457e8d320d82cb110773be7
Ubuntu <ubuntu@rails-dev-box>
2017-02-24 03:58:18 +0000

    add read_at to message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,9 @@
 class Message < ApplicationRecord
+  belongs_to :sender, class_name: 'User'
+  belongs_to :recipient, class_name: 'User'
+
   def sender
     User.find_by_id(sender_id) || User.new(email: "nobody@but.you")
   end
+  
 end
